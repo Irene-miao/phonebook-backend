a1281f8022f688ea6d13a4c6010124f16fce8d70
Irene L <irene_lee@live.com.sg>
2021-07-18 21:42:18 +0800

    Added MongDB

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # phonebook-backend
-Simple Node js backend server hosted on Heroku
+Simple Node js backend server hosted on Heroku:
+https://hidden-brook-55950.herokuapp.com/
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { response } = require('express');
+
 const express = require('express');
 const app = express();
-app.use(express.json());
+require('dotenv').config();
+const Phone = require('./models/phone');
+
 const morgan = require('morgan');
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 // Create morgan token for body
@@ -9,101 +11,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :
 const cors = require('cors');
 app.use(cors());
 app.use(express.static('build'));
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
-// Get number of persons and request date
-app.get('/info', (request, response) => {
-   response.send(`<p>Phonebook has info for ${persons.length} people</p> <br> <p>${new Date()}</p>`);
-  
-});
-
-// GET all persons
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-});
-
-// Get one person id
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end(`ID ${id} not found!`)
-  };
-});
-
-// Delete one person id
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  response.status(204).end();
-});
-
-// Post a new person
-app.post('/api/persons', (request, response) => {
-  
-  const generateId = (max) => {
-const id = persons.length > 0 ? Math.floor(Math.random() * max) : 0;
-return id 
-  };
-
-  const person = request.body;
-
-  if (!person.name) {
-    return response.status(400).json({
-      error:'name missing'
-    })
-  };
-
-  if (!person.number) {
-    return response.status(400).json({
-    error: 'number missing'
-  })
-  };
-
-  const name = persons.map(person => person.name);
-  console.log(name);
-  d = new Date();
-  if (name.map(item => item !== person.name)) {
-    const personDetail = {
-      name: person.name,
-      number: person.number,
-      date: d.toLocaleString(),
-      id: generateId(100),
-    };
-    persons = persons.concat(personDetail);
-    response.json(personDetail);
-    
-  } else {
-    return response.status(400).json({
-      error: 'name already exist'
-  });
-};
-});
+app.use(express.json());
 
 // Middleware that print information about every request sent to server
 const requestLogger = (request, response, next) => {
@@ -115,14 +23,136 @@ const requestLogger = (request, response, next) => {
 };
 app.use(requestLogger);
 
+// Mongoose database
+/*if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1)
+};*/
+
+//const password = process.argv[2];
+// Get the input from the console to add to database
+/*const name = process.argv[3];
+const number = process.argv[4];
+
+const phone = new Phone({
+  name: name,
+  number: number,
+});
+
+if (process.argv.length > 3) {
+  phone.save().then(result => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`);
+      mongoose.connection.close()
+  });
+} else {
+Phone.find({}).then(contact => {
+  console.log('phonebook:');
+  contact.map(item => {
+      console.log(item.name, item.number);
+  });
+  mongoose.connection.close()
+});
+};*/
+
+// Get number of persons and request date
+app.get('/info', (request, response, next) => {
+Phone.count({}).then(contact => {
+  response.json(`Phonebook has info for ${contact} people ${new Date()}`);
+})
+.catch(error => next(error))
+});
+
+// GET all persons
+app.get('/api/persons', (request, response) => {
+  Phone.find({}).then(contact => {
+    response.json(contact)
+  });
+});
+
+// Get one person id
+app.get('/api/persons/:id', (request, response) => {
+ Phone.findById(request.params.id)
+ .then(phone => {
+   if (phone) {
+    response.json(phone)
+   } else {
+     response.status(404).end()
+   }
+ })
+ .catch(error =>  next(error))
+});
+
+// Delete one person id
+app.delete('/api/persons/:id', (request, response, next) => {
+  Phone.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+});
+
+// Post a new person
+app.post('/api/persons', (request, response, next) => {
+  
+  const person = request.body;
+
+    const phone = new Phone({
+      name: person.name,
+      number: person.number,
+      date: new Date(),
+    });
+
+   phone.save()
+   .then(savedPhone => {
+     console.log(savedPhone);
+   return savedPhone.toJSON()  // Received object from Mongoose and format it
+   })
+   .then(savedAndFormattedPhone => {
+     response.json(savedAndFormattedPhone)  // Access the formatted phone
+   })
+   .catch(error => next(error))
+});
+
+// Update a person number
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = request.body;
+
+  const phone = {
+    number: person.number,
+  }
+
+  Phone.findByIdAndUpdate(request.params.id, phone, {new: true})
+  .then(updatedPhone => {
+    response.json(updatedPhone)
+  })
+  .catch(error => next(error))
+});
+
 // Middleware that catch requests made to non-existing routes
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
 };
 app.use(unknownEndpoint);
 
+// Middleware that catch errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  // the error was caused by an invalid object id for Mongo
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {   // input does not pass validators eg: minLength not met
+return response.status(400).json({ error: error.message })
+  }
+  next(error)
+};
+
+// // this has to be the last loaded middleware
+app.use(errorHandler);
+
 //  Use the port defined in environment variable PORT or 
 // port 3001 if the environment variable PORT is undefined
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
